253c84dfcaf4b634cc06bea61cbaba022b78dc41
Cooper Raterink <craterink@utexas.edu>
2016-02-28 17:49:03 -0600

    Set up project project euler

---
+++ ProjectProjectEuler/C#/EulerLib/Functions.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.IO;
+
+namespace EulerLib
+{
+    //TODO: Implement "best times" Json file
+    public static class Euler
+    {
+        public delegate String ProblemMethod();
+
+        public static void RunProblem(ProblemMethod problem)
+        {
+            Stopwatch time = new Stopwatch();
+            time.Start();
+
+            String output = problem();
+
+            time.Stop();
+            Console.WriteLine("Problem: " + Regex.Match(problem.Method.ToString(), "[a-zA-Z0-9]+\\(").ToString().Replace("(", ""));
+            Console.WriteLine("Output: " + output);
+            Console.WriteLine("Time Elapsed: " + time.ElapsedMilliseconds + " ms");
+            Console.ReadKey();
+        }
+    }
+
+    public static class Primes
+    {
+        public static List<long> findPrimes(Func<long, long, bool> logicFunc)
+        {
+            List<long> primes = new List<long>();
+            primes.Add(2);
+            long index = 2, cand = 3;
+            while (logicFunc(index, cand))
+            {
+                bool p = true;
+                foreach(long prime in primes)
+                {
+                    if (Fn.isFactor(prime, cand)) {
+                        p = false;
+                        break;
+                    }
+                }
+                if(p)
+                {
+                    primes.Add(cand);
+                    index += 1;
+                }
+                cand += 2;
+            }
+            return primes;
+        }
+
+        public static List<long> primeFactors(long number)
+        {
+            List<long> factors = Fn.findFactors(number);
+            List<long> primes = findPrimes((n, pr) => pr <= factors.Last());
+            return primes.Intersect(factors).ToList();
+        }
+
+        public static long nthPrime(long n)
+        {
+            return findPrimes((ind, pr) => ind <= n).Last();
+        }
+
+        public static bool isPrime(long number)
+        {
+            List<long> primesRange = findPrimes((ind, pr) => pr <= Math.Sqrt(number));
+            for (int i = 0; i < primesRange.Count; i++)
+            {
+                if (Fn.isFactor(primesRange.ElementAt(i), number)) return false;
+            }
+            return true;
+        }
+    }
+
+    public static class Fn
+    {
+        /// <summary>
+        /// Returns a certain indexed fibonacci number
+        /// </summary>
+        /// <param name="index">Index (where first Fib number is index 1)</param>
+        /// <returns></returns>
+        public static int fib(int index)
+        {
+            if (index < 3) return 1;
+            int back1 = 1, back2 = 1, current = 0;
+            for (int i = 3; i <= index; i++)
+            {
+                current = back1 + back2;
+                back2 = back1;
+                back1 = current;
+            }
+            return current;
+        }
+
+        /// <summary>
+        /// Is a number even
+        /// </summary>
+        /// <param name="num">Any integer num</param>
+        /// <returns>Whether num is even</returns>
+        public static bool even(int num)
+        {
+            return (num / 2) == ((double)num) / 2.0;
+        }
+
+        public static List<long> findFactors(long number)
+        {
+            List<long> factors = new List<long>();
+            long end = number;
+            for (long i = 2; i < end; i += 1)
+            {
+                if (isFactor(i, number))
+                {
+                    factors.Add(i);
+                    factors.Add(number / i);
+                    end = number / i;
+                }
+            }
+            factors.Sort();
+            return factors;
+        }
+        public static bool isFactor(long factor, long number)
+        {
+            return number % factor == 0;
+        }
+
+        public static bool isPalindromic(long number)
+        {
+            Char[] num = Math.Abs(number).ToString().ToCharArray();
+            int length = num.Length;
+            for(int i = 0; i <= length / 2; i++)
+            {
+                if (!num[i].Equals(num[length - i - 1])) return false;
+            }
+            return true;
+        }
+
+        public static long sumRange(long start, long end)
+        {
+            long sum = 0;
+            for(long i = start; i <= end; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+        public static long sumSquaresInRange(long start, long end)
+        {
+            long sum = 0;
+            for (long i = start; i <= end; i++)
+            {
+                sum += i*i;
+            }
+            return sum;
+        }
+
+        public static bool isPythagTriplet(long a, long b, long c)
+        {
+            return a * a + b * b == c * c;
+        }
+        
+        public static long prod13digits(String thirteen)
+        {
+            long prod = 1;
+            foreach(char c in thirteen.ToCharArray())
+            {
+                prod *= Int32.Parse(c.ToString());
+            }
+            return prod;
+        }
+    }
+
+    public static class IO
+    {
+        public static String getTextFile(String fileName)
+        {
+            return File.ReadAllText(@"C:\Users\Cooper\Documents\Programming\Project Euler\Euler\" + fileName);
+        }
+    }
+
+}
---
+++ ProjectProjectEuler/C#/Problems1-10/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using EulerLib;
+
+namespace Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Euler.RunProblem(Problem1);
+            //Euler.RunProblem(Problem2);
+            //Euler.RunProblem(Problem3);
+            //Euler.RunProblem(Problem4);
+            //Euler.RunProblem(Problem5);
+            //Euler.RunProblem(Problem7);
+            Euler.RunProblem(Problem8);
+            //Euler.RunProblem(Problem9);
+            //Euler.RunProblem(Problem10);
+        }
+
+        //TODO
+        public static String Problem1()
+        {
+            return "";
+        }
+
+        public static String Problem2()
+        {
+            int MAX = 4000000;
+
+            int value = 0, sum = 0, i = 3;
+            while (value <= MAX)
+            {
+                value = Fn.fib(i++);
+                if (value > MAX) break;
+                if (Fn.even(value))
+                {
+                    sum += value;
+                }
+            }
+
+            return sum.ToString();
+        }
+
+        public static String Problem3()
+        {
+            long NUM = 600851475143;
+            long NUM_T = 1319573735;
+
+            List<long> factors = Fn.findFactors(NUM);
+            return factors.FindLast(f => Primes.isPrime(f)).ToString();
+        }
+
+        public static String Problem4()
+        {
+            long largest = 0;
+            for(int i = 999; i >= 100; i--)
+            {
+                for (int j = 999; j >= 100; j--)
+                {
+                    if(i*j > largest && Fn.isPalindromic(i*j))
+                    {
+                        largest = i * j;
+                    }
+                }
+            }
+            return largest.ToString();
+        }
+
+        public static String Problem5()
+        {
+            //TODO: implement divisor methods?
+            return (5 * 7 * Math.Pow(3, 2) * 11 * 13 * Math.Pow(2,4) * 17 * 19).ToString();
+        }
+
+        public static String Problem6()
+        {
+            return (Math.Pow(Fn.sumRange(1, 100), 2) - Fn.sumSquaresInRange(1, 100)).ToString();
+        }
+
+        public static String Problem7()
+        {
+            List<long> list = Primes.findPrimes((n, pr) => n <= 10001);
+            return list.Last().ToString();
+        }
+
+        public static String Problem8()
+        {
+            String number = IO.getTextFile("Problem8.txt");
+            number = number.Replace("\r\n", "");
+            long max = 0;
+            for(int i = 0; i < number.Length - 13; i++)
+            {
+                if ((Fn.prod13digits(number.Substring(i, 13))) > max)
+                {
+                    max = Fn.prod13digits(number.Substring(i, 13));
+                }
+            }
+            return max.ToString();
+        }
+
+        public static String Problem9()
+        {
+            for(int i = 1; i <= 1000; i++)
+            {
+                for (int j = i; j <= 1000; j++)
+                {
+                    for (int k = j; k <= 1000; k++)
+                    {
+                        if (i + j + k == 1000 && Fn.isPythagTriplet(i, j, k))
+                            return "" + i*j*k;
+                    }
+                }
+            }
+            return "Nothing found.";
+        }
+
+        public static String Problem10()
+        {
+            List<long> list = Primes.findPrimes((n, pr) => pr < 2000000);
+            long sum = 0;
+            foreach(long l in list)
+            {
+                sum += l;
+            } 
+            return sum.ToString();
+        }
+    }
+}
